f0b8324c1d88736591cbc2f1f2df8cad5533adeb
ChineduNwosisi <izuchu603@gmail.com>
2022-09-11 03:17:49 -0700

    C task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha == 'e' || alpha == 'q')
+		{
+			continue;
+		}
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
